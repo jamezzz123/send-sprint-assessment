719ccad643751546949b154277b4b017972b6030
jamezzz123 <chinwokwuj@gmail.com>
2023-11-09 00:46:45 +0100

    refactor: added types to services files

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,12 @@
 // services/api.js
 const API_URL = process.env.API_BASE_URL;
-
-export async function getPost(id) {
+export type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+export async function getPost(id: number | string): Promise<Post> {
     try {
         const response = await fetch(`${API_URL}/posts/${id}`);
         const data = await response.json();
@@ -11,7 +16,7 @@ export async function getPost(id) {
         throw error;
     }
 }
-export async function getPosts() {
+export async function getPosts(): Promise<Post[]> {
     try {
         const response = await fetch(`${API_URL}/posts`);
         const data = await response.json();
