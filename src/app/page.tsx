3fbeadf21adcc549777e2b7cedff4cb31bb9b056
jamezzz123 <chinwokwuj@gmail.com>
2023-11-09 14:27:57 +0100

    refactor: implemeted the search bar for blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import Pagination from "@/components/Pagination";
+import SearchBar from "@/components/SearchBar";
 import { Post, getPosts } from "@/services";
 import { useEffect, useState } from "react";
-
 
 export default function Home() {
   const [data, setData] = useState<Post[]>([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [currentItems, setCurrentItems] = useState<Post[]>([]);
+  const [filteredData, setFilteredData] = useState<Post[]>([]);
   async function fetchPost() {
     try {
       let data = await getPosts();
@@ -19,10 +20,14 @@ export default function Home() {
     }
   }
 
-  function onPageChange(page: number){
+  function onPageChange(page: number, search = "") {
     const indexOfLastItem = page * 10;
     const indexOfFirstItem = indexOfLastItem - 10;
-    const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+    const filteredData = data.filter((post) =>
+      post.title.toLowerCase().includes(search.toLowerCase())
+    );
+    const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+    setFilteredData(filteredData);
     setCurrentItems(currentItems);
     setCurrentPage(page);
   }
@@ -31,28 +36,33 @@ export default function Home() {
     fetchPost();
   }, []);
 
-  if(loading){
-    return <p>Loading...</p>
+  if (loading) {
+    return <p>Loading...</p>;
   }
 
   return (
     <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white">
       <div className="flex justify-between flex-col px-4 mx-auto max-w-screen-xl ">
         <div className="mx-auto mb-3 w-full max-w-2xl">
-          {/* <ul className="list-decimal px-3 mx-5"> */}
-            {currentItems.map((post, index: number) => (
-              <h3
-                key={index}
-                className="mb-4 my-5 text-xl  font-medium leading-tight text-gray-900 lg:mb-3 lg:text-2xl"
-              > {post.id})  
-                {" "}{post.title}
-              </h3>
-            ))}
-          {/* </ul> */}
+          <SearchBar
+            onChangeInput={(search: string) =>
+              onPageChange(currentPage, search)
+            }
+          />
+
+          {currentItems.map((post, index: number) => (
+            <h3
+              key={index}
+              className="mb-4 my-5 text-xl  font-medium leading-tight text-gray-900 lg:mb-3 lg:text-2xl"
+            >
+              {" "}
+              {post.id}) {post.title}
+            </h3>
+          ))}
         </div>
         <Pagination
           onPageChange={onPageChange}
-          totalItems={data.length}
+          totalItems={filteredData.length}
           currentPage={currentPage}
           perPage={10}
         />
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+export default function SearchBar({ onChangeInput }: any) {
+  const [search, setSearch] = useState("");
+
+  const handleInputChange = (e: any) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onChangeInput(search);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [search]);
+  return (
+    <form>
+      <div className="flex mb-6 space-x-2">
+        <div className="relative flex-1">
+          <input
+            type="text"
+            value={search}
+            onChange={handleInputChange}
+            className="text-black border text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
+            placeholder="Search by keyword"
+            required
+          />
+        </div>
+        <button className="flex items-center justify-center px-6 py-3  text-base font-medium text-blue-600 bg-white rounded-lg hover:bg-blue-200 disabled:text-gray-400">
+          Clear
+        </button>
+      </div>
+    </form>
+  );
+}
