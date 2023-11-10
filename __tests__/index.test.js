6fe9974bee4554caaad62972c5e38a68f2eb3e64
jamezzz123 <chinwokwuj@gmail.com>
2023-11-10 16:54:52 +0100

    refactor: implemented test for Search Bar components

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,46 @@
-// import { render, screen } from '@testing-library/react'
-// import Home from '../pages/index'
-import '@testing-library/jest-dom'
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import SearchBar from "@/components/SearchBar";
 
-describe('Home', () => {
-    it('add two numbers', () => {
-        expect(1 + 1).toBe(2);
-    })
-})
+describe("SearchBar Component", () => {
+  it("should change input value", () => {
+    const onChangeInput = jest.fn();
+    const { getByPlaceholderText } = render(<SearchBar onChangeInput={onChangeInput} />);
+
+    const input = getByPlaceholderText("Search by keyword");
+    fireEvent.change(input, { target: { value: "test input" } });
+
+    expect(input.value).toBe("test input");
+  });
+
+  it("should call onChangeInput after a delay", async () => {
+    jest.useFakeTimers();
+    const onChangeInput = jest.fn();
+    const { getByPlaceholderText } = render(<SearchBar onChangeInput={onChangeInput} />);
+
+    const input = getByPlaceholderText("Search by keyword");
+    fireEvent.change(input, { target: { value: "test" } });
+
+    jest.advanceTimersByTime(500);
+
+    await waitFor(() => {
+      expect(onChangeInput).toHaveBeenCalledWith("test");
+    });
+  });
+
+  it("should clear input value on button click", () => {
+    const onChangeInput = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <SearchBar onChangeInput={onChangeInput} />
+    );
+
+    const input = getByPlaceholderText("Search by keyword");
+    const clearButton = getByText("Clear");
+
+    fireEvent.change(input, { target: { value: "test input" } });
+    expect(input.value).toBe("test input");
+
+    fireEvent.click(clearButton);
+    expect(input.value).toBe("");
+  });
+});
