41940d7bb62990237d83c84904948afae6ce581b
jamezzz123 <chinwokwuj@gmail.com>
2023-11-10 16:42:22 +0100

    feat(testing): added setup fro testing

---
+++ __tests__/index.test.js
@@ -0,0 +1,9 @@
+// import { render, screen } from '@testing-library/react'
+// import Home from '../pages/index'
+import '@testing-library/jest-dom'
+
+describe('Home', () => {
+    it('add two numbers', () => {
+        expect(1 + 1).toBe(2);
+    })
+})
